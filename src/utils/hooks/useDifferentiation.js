63ac6c6c11248823bdb392c01f21292440923f7b
Xuheng Liu <liuxuheng1990@gmail.com>
2019-11-27 23:58:35 +0800

    feat: add useEventListner useHistory useWhyDidYouUpdate and fix problem with useCompare

--- src/utils/hooks/useDifferentiation.js
+++ src/utils/hooks/useDifferentiation.js
@@ -2,7 +2,7 @@ import { differenceWith, isEqual } from 'lodash-es';
 import usePrevious from './usePrevious';
 
 // 深度比较两次的prop，找到新增和删除部分
-export default function(val) {
+export default function useDifferentiation(val) {
   const prevVal = usePrevious(val);
   const added = differenceWith(val, prevVal, isEqual);
   const removed = differenceWith(prevVal, val, isEqual);
--- src/utils/hooks/usePrevious.js
+++ src/utils/hooks/usePrevious.js
@@ -1,7 +1,7 @@
 import { useRef, useEffect } from 'react';
 
 // return previous value
-export default function(value) {
+export default function usePrevious(value) {
   const ref = useRef();
   useEffect(() => {
     ref.current = value;

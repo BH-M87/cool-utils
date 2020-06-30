b1e0105e84ba9e8fd2c28b578042445ad28ac9d7
Xuheng Liu <liuxuheng1990@gmail.com>
2020-06-30 20:14:49 +0800

    feat: temp save typescript file

---
+++ typescript/axios/libs/setDefaults.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+// headers
+axios.defaults.headers.common = {
+  ...axios.defaults.headers.common,
+  'x-requested-with': 'XMLHttpRequest',
+};
+// axios.defaults.headers.get = {
+//   ...axios.defaults.headers.get,
+// };
+// axios.defaults.headers.post = {
+//   ...axios.defaults.headers.post,
+// };
+// axios.defaults.headers.delete = {
+//   ...axios.defaults.headers.delete,
+// };
+// axios.defaults.headers.put = {
+//   ...axios.defaults.headers.put,
+// };
+// withCredentials
+axios.defaults.withCredentials = true;

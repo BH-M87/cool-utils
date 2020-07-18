608ed45d9581a56d58c288a4a51a85610876d2a0
Xuheng Liu <liuxuheng1990@gmail.com>
2020-07-18 19:49:15 +0800

    feat: update forEach method

--- src/utils/forEach.ts
+++ src/utils/forEach.ts
@@ -12,35 +12,40 @@ import isArray from './isArray';
  * @param {Object|Array} obj The object to iterate
  * @param {Function} fn The callback to invoke for each item
  */
+function forEach<T>(obj: T[], fn: (value: T, key: number, obj: T[]) => void): T[];
+function forEach<T>(obj: T, fn: (value: T, key: number, obj: T) => void): T;
 function forEach(obj: AnyObject, fn: (value: any, key: string, obj: AnyObject) => void): AnyObject;
-function forEach(obj: any[], fn: (value: any, key: number, obj: any[]) => void): any[];
 function forEach(obj: any, fn: Function): any {
   // Don't bother if no value provided
   if (obj === null || typeof obj === 'undefined') {
     return obj;
   }
 
+  let newObj: AnyObject;
+
   // Force an array if not already something iterable
   if (typeof obj !== 'object') {
     /*eslint no-param-reassign:0*/
-    obj = [obj];
+    newObj = [obj];
   }
 
   if (isArray(obj)) {
+    newObj = [];
     // Iterate over array values
     for (let i = 0, l = obj.length; i < l; i += 1) {
-      fn.call(null, obj[i], i, obj);
+      newObj[i] = fn.call(null, obj[i], i, obj);
     }
   } else {
+    newObj = {};
     // Iterate over object keys
     // eslint-disable-next-line no-unused-vars
     for (const key in obj) {
       if (Object.prototype.hasOwnProperty.call(obj, key)) {
-        fn.call(null, obj[key], key, obj);
+        newObj[key] = fn.call(null, obj[key], key, obj);
       }
     }
   }
-  return obj;
+  return newObj;
 }
 
 export default forEach;

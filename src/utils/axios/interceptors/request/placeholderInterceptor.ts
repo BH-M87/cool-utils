700be54829a0d1fb4ed5915d2150822d40037916
Xuheng Liu <liuxuheng1990@gmail.com>
2020-07-24 12:15:40 +0800

    fix: fix bug with placeholderInterceptor

--- src/utils/axios/interceptors/request/placeholderInterceptor.ts
+++ src/utils/axios/interceptors/request/placeholderInterceptor.ts
@@ -1,17 +1,18 @@
 import { AxiosInstance } from 'axios';
 import replacePlaceholder from '../../../replacePlaceholder';
+import isObject from '../../../isObject';
 
 export default (axios: AxiosInstance) => {
   axios.interceptors.request.use(
     function(config) {
       let { url, params, data } = config;
-      if (typeof params === 'object') {
+      if (isObject(params)) {
         ({ string: url, data: params } = replacePlaceholder(url, /:\w+/g, params));
       }
-      if (typeof data === 'object') {
+      if (isObject(data)) {
         ({ string: url, data } = replacePlaceholder(url, /:\w+/g, data));
       }
-      return config;
+      return { ...config, url, params, data };
     },
     function(error) {
       return Promise.reject(error);
